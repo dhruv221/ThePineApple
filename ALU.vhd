d0a5adbe197eda2b85a29cd4184e22ac2ed201c8
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-04-22 19:26:06 +0530

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -6,9 +6,10 @@ use ieee.NUMERIC_STD.all;
 
 
 entity ALU is
-  Port (A: in std_logic_vector (7 downto 0);
-        B: in std_logic_vector (7 downto 0);
+  Port (--inouts
         ALU_out_bus: out std_logic_vector(7 downto 0);
+        A: in std_logic_vector (7 downto 0);
+        B: in std_logic_vector (7 downto 0);
         N: out std_logic;
         C: out std_logic;
         Z: out std_logic;
@@ -22,40 +23,38 @@ end ALU;
 
 architecture Behavioral of ALU is
 signal ALU_value : std_logic_vector(7 downto 0);
---signal B_knot : std_logic_vector(7 downto 0);
 signal carry : std_logic;
 signal negetive : std_logic;
 begin
 
 process(A, B, SorD) is
-variable a_dash : unsigned(8 downto 0) := unsigned('0' & A);
-variable b_dash : unsigned(8 downto 0) := unsigned('0' & B);
-variable temp : unsigned(8 downto 0);
-begin
-temp := a_dash + b_dash;
-if (SorD = '0') then
-    ALU_value <= std_logic_vector(temp(7 downto 0));
-    --ALU_out_bus <= ALU_value;
-elsif (SorD = '1') then
-    ALU_value <= A - B;
-end if;
-C <= std_logic(temp(8));
-N <= ALU_value(7);
-if (ALU_value = "00000000") then
-    Z <= '1';
-else
-    Z <= '0';
-end if;
+    variable a_dash : unsigned(8 downto 0) := unsigned('0' & A);
+    variable b_dash : unsigned(8 downto 0) := unsigned('0' & B);
+    variable temp : unsigned(8 downto 0);
+    begin
+    temp := a_dash + b_dash;
+    if (SorD = '0') then
+        ALU_value <= std_logic_vector(temp(7 downto 0));
+        --ALU_out_bus <= ALU_value;
+    elsif (SorD = '1') then
+        ALU_value <= A - B;
+    end if;
+    C <= std_logic(temp(8));
+    N <= ALU_value(7);
+    if (ALU_value = "00000000") then
+        Z <= '1';
+    else
+        Z <= '0';
+    end if;
 end process;  
 
 process(ALU_O) is
 begin
-if (ALU_O = '1') then
-    ALU_out_bus <= ALU_value;
-else 
-    ALU_out_bus <= "ZZZZZZZZ";
-end if;
+    if (ALU_O = '1') then
+        ALU_out_bus <= ALU_value;
+    else 
+        ALU_out_bus <= "ZZZZZZZZ";
+    end if;
 end process;
-
 
 end Behavioral;
