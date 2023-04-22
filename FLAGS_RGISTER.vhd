1aed22d563904578945445b3c59850a6adf4a29e
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-04-22 19:27:54 +0530

    Update FLAGS_RGISTER.vhd

--- FLAGS_RGISTER.vhd
+++ FLAGS_RGISTER.vhd
@@ -4,13 +4,14 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.NUMERIC_STD.all;
 
 entity FLAGS_REGISTER is
-  Port( N : in STD_LOGIC;
-        C : in STD_LOGIC;
-        Z : in STD_LOGIC;
-        FLAGS : out STD_LOGIC_VECTOR(2 DOWNTO 0);
+  Port(--inouts 
+       FLAGS : out STD_LOGIC_VECTOR(2 DOWNTO 0);
+       N : in STD_LOGIC;
+       C : in STD_LOGIC;
+       Z : in STD_LOGIC; 
        --Control Signals
-        FI : in STD_LOGIC;
-        CLK : in STD_LOGIC);
+       FI : in STD_LOGIC;
+       CLK : in STD_LOGIC);
 end FLAGS_REGISTER;
 
     
@@ -19,11 +20,11 @@ begin
   
 process(CLK, FI)
 begin
-  if (rising_edge(CLK) and FI = '1') then
-    FLAGS(0) <= Z;
-    FLAGS(1) <= C;
-    FLAGS(2) <= N;
-  end if;
+    if (rising_edge(CLK) and FI = '1') then
+        FLAGS(0) <= Z;
+        FLAGS(1) <= C;
+        FLAGS(2) <= N;
+    end if;
 end process;
 
 end Behavioral;
