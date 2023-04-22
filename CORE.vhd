0c3605403e25af66311427d44a643a6aaceefac0
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-04-22 19:33:34 +0530

    Update CORE.vhd
    added all modules except MAR, OUTPUT REGISTER

--- CORE.vhd
+++ CORE.vhd
@@ -9,8 +9,15 @@ entity CORE is
     
 end CORE;
 
-architecture Behavioral of CORE is
 
+
+
+
+
+
+
+architecture Behavioral of CORE is
+-- A register
 component A_REGISTER is
     Port ( AI : in STD_LOGIC;
            AO : in STD_LOGIC;
@@ -20,7 +27,7 @@ component A_REGISTER is
            CLK: in STD_LOGIC;
            RST : in STD_LOGIC);
 end component;
-
+-- B register
 component B_REGISTER is
     Port ( BI : in STD_LOGIC;
            BO : in STD_LOGIC;
@@ -30,7 +37,7 @@ component B_REGISTER is
            CLK: in STD_LOGIC;
            RST : in STD_LOGIC);
 end component;
-
+-- ALU
 component ALU is
     Port (A: in std_logic_vector (7 downto 0);
           B: in std_logic_vector (7 downto 0);
@@ -42,28 +49,121 @@ component ALU is
           SorD: in std_logic;
           ALU_O: in std_logic);
 end component;
+-- FLAGS register
+component FLAGS_REGISTER is 
+    Port(--inouts 
+         FLAGS : out STD_LOGIC_VECTOR(2 DOWNTO 0);
+         N : in STD_LOGIC;
+         C : in STD_LOGIC;
+         Z : in STD_LOGIC; 
+         --Control Signals
+         FI : in STD_LOGIC;
+         CLK : in STD_LOGIC);
+end component;
+-- INSTRUCTION register
+component INSTRUCTION_REGISTER is
+  Port(--inouts 
+       IR_out : out STD_LOGIC_VECTOR(7 downto 0);
+       IR_in : in STD_LOGIC_VECTOR(7 downto 0);
+       --Control Signals
+       II : in STD_LOGIC; 
+       CLK : in STD_LOGIC);    
+end component;
+-- STEP counter 
+component STEP_COUNTER is
+    port(--inouts
+         step_count : out std_logic_vector(3 downto 0);
+         --control signals 
+         SCR : in std_logic;
+         CLK : in std_logic);
+end component;
+-- PROGRAM counter 
+component PROGRAM_COUNTER is
+    port(--inouts
+         PC_inout_bus : inout std_logic_vector(7 downto 0);
+         carryout : out std_logic;
+         --control signals
+         C_INC : in std_logic;
+         CI : in std_logic;
+         CO : in std_logic;
+         CLK : in std_logic;
+         RST : in std_logic);
+end component;
+-- MEMORY ADDRESS register 
+component MEMADDR_REGISTER is
+    port (--inouts
+          MAR_in_bus : in std_logic_vector(7 downto 0);
+          MAR_out : out std_logic_vector(7 downto 0);
+          --control signal
+          MARI : in std_logic;
+          CLK : in std_logic);
+end component;
 
-signal A_IN, A_OUT, B_IN, B_OUT, SorD, ALU_O : std_logic;
-signal A, B : std_logic_vector(7 downto 0);
+
+
+
+signal AI, AO, BI, BO, SorD, ALUO, FI, II, SCR, HorL, CE, CI, CO : std_logic;
+signal CIL, CIH, COL, COH : std_logic;
+signal N, C, Z : std_logic;
+signal A, B, INSTRUCTION : std_logic_vector(7 downto 0);
+signal FLAGS : std_logic_vector(2 downto 0);
+signal STEP_COUNT : std_logic_vector(3 downto 0);
+signal PC_carry : std_logic;
 begin
 A_REG: A_REGISTER port map (A_inout_bus => DATA_BUS, 
                             CLK => CLK,
                             RST => RST,
-                            AI => A_IN,
-                            AO => A_OUT,
+                            AI => AI,
+                            AO => AO,
                             A_value => A);
                             
 B_REG: B_REGISTER port map (B_inout_bus => DATA_BUS, 
                             CLK => CLK,
                             RST => RST,
-                            BI => B_IN,
-                            BO => B_OUT,
+                            BI => BI,
+                            BO => BO,
                             B_value => B);
                             
 ALU_M : ALU port map (A => A,
-                    B => B,
-                    ALU_out_bus => DATA_BUS,
-                    SorD => SorD,
-                    ALU_O => ALU_O);                          
+                      B => B,
+                      ALU_out_bus => DATA_BUS,
+                      SorD => SorD,
+                      ALU_O => ALUO,
+                      N => N,
+                      C => C,
+                      Z => Z);     
+                    
+FLAGS_REG : FLAGS_REGISTER port map (--inouts 
+                                     FLAGS => FLAGS,
+                                     N => N,
+                                     C => C,
+                                     Z => Z,
+                                     --Control Signals
+                                     FI => FI,
+                                     CLK => CLK);                   
+
+I_REG : INSTRUCTION_REGISTER port map (IR_in => DATA_BUS,
+                                       IR_out => INSTRUCTION,
+                                       II => II,
+                                       CLK => CLK);
+                                       
+STEP : STEP_COUNTER port map (step_count => STEP_COUNT,
+                              SCR => SCR,
+                              CLK => CLK);
+
+PCL : PROGRAM_COUNTER port map (PC_inout_bus => DATA_BUS,
+                                carryout => PC_carry,
+                                C_INC => CE,
+                                CI => CIL,
+                                CO => COL,
+                                CLK => CLK,
+                                RST => RST);
+                                
+PCH : PROGRAM_COUNTER port map (PC_inout_bus => DATA_BUS,
+                                C_INC => PC_carry,
+                                CI => CIH,
+                                CO => COH,
+                                CLK => CLK,
+                                RST => RST);
 
 end Behavioral;
