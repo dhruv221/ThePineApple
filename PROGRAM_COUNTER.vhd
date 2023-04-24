d3981a9b3341d86e8e35ae739720f0fa810e19f6
dhruv221 <62287269+dhruv221@users.noreply.github.com>
2023-04-24 13:12:13 +0530

    Update PROGRAM_COUNTER.vhd

--- PROGRAM_COUNTER.vhd
+++ PROGRAM_COUNTER.vhd
@@ -6,10 +6,11 @@ use ieee.NUMERIC_STD.all;
 
 entity PROGRAM_COUNTER is
     port(--inouts
-         PC_inout_bus : inout std_logic_vector(7 downto 0);
+         PC_in_bus : in std_logic_vector(7 downto 0);
+         PC_out_bus : out std_logic_vector(7 downto 0);
          carryout : out std_logic;
          --control signals
-         C_INC : in std_logic;
+         CE : in std_logic;
          CI : in std_logic;
          CO : in std_logic;
          CLK : in std_logic;
@@ -19,34 +20,43 @@ end entity;
 
 
 architecture Behavioral of PROGRAM_COUNTER is
-signal PC_value : std_logic_vector (7 downto 0);
+signal PC_value : std_logic_vector(7 downto 0);
+
+begin
+process(CLK, RST)
 begin
 
-process(CLK, RST, C_INC, CI, CO)
-begin
-    if (C_INC = '1' and rising_edge(CLK)) then
-    PC_value <= PC_value + "0001";
-    end if;
-    
-    if (CI = '1' and rising_edge(CLK)) then
-    PC_value <= PC_inout_bus;
-    end if;
-    
-    if (CO = '1') then
-    PC_inout_bus <= PC_value(7 downto 0);
-    else
-    PC_inout_bus <= (others => 'Z');
-    end if;
-    
-    if (PC_value = "11111111") then
-    carryout <= '1';
-    else
-    carryout <= '0';
-    end if;
-    
-    if (RST = '1') then
+if (RST = '1') then
     PC_value <= "00000000";
+else
+    if(rising_edge(CLK)) then
+        if (CE = '1' and CI = '0') then
+            PC_value <= PC_value + "00000001";
+        elsif (CI = '1' and CE = '0') then
+            PC_value <= PC_in_bus;        
+          else
+            PC_value <= PC_value;          
+        end if;
     end if;
+end if;
+
+if (PC_value = "11111111") then
+    carryout <= '1';
+else
+    carryout <= '0';
+end if;
+
+end process;
+
+process(CO)
+begin
+
+if (CO = '1') then
+    PC_out_bus <= PC_value(7 downto 0);
+else
+    PC_out_bus <= (others => 'Z');
+end if;
+
 end process;
 
 end Behavioral;
